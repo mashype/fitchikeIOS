3ec72623b1748b9fe70384a032eb8b7f2f84397e
mashype <mashype@me.com>
2018-01-13 14:46:32 -0800

    Initial Commit

---
+++ fitchike/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fitchike
+//
+//  Created by Seth Merritt on 1/13/18.
+//  Copyright © 2018 leftyseth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
